f6ce32a6a1c66ff1e1c57e2166a0f43c41d630b6
bspp1314 <linyuanpeng1314@gmail.com>
2019-08-29 14:17:42 +0800

    update

--- 438/438.go
+++ 438/438.go
@@ -8,62 +8,47 @@ func findAnagrams(s string, p string) []int {
 	if sLen == 0 || pLen == 0 {
 		return nil
 	}
-	pMap := make([]int, 256)
-	window := make([]int, 256)
-	cn := 0
+	needs := make(map[byte]int)
 	for i := 0; i < pLen; i++ {
-		if pMap[p[i]] == 0 {
-			cn++
-		}
-		pMap[p[i]]++
+		needs[p[i]]++
 	}
-	left := 0
+	windows := make(map[byte]int)
 	right := 0
-	formed := 0
+	left := 0
+	match := 0
 	res := make([]int, 0)
-
 	for right < sLen {
-		if pMap[s[right]] == 0 {
-			window = make([]int, 256)
-			formed = 0
-			right++
-			left = right
-		} else {
-			if window[s[right]] == 0 {
-				formed++
-			}
-			if right == 2 {
-				fmt.Println("left", left)
-				fmt.Println("right", left)
-				fmt.Println("formed", formed)
-				fmt.Println("cn", cn)
-			}
-			window[s[right]]++
-			if window[s[right]] <= pMap[s[right]] {
-				if formed == cn {
-					window[s[left]]--
-					formed--
-					res = append(res, left)
-					right++
-					left++
-				} else { //formed < cn
-					right++
-				}
-			} else { //window[s[right]] > pMap[s[right]]
-				window[s[left]]--
-				left++
-				right++
-
+		c := s[right]
+		if needs[c] > 0 {
+			windows[c]++
+			if windows[c] == needs[c] {
+				match++
 			}
 		}
+		right++
+		for match == len(needs) {
+			if right-left == pLen {
+				res = append(res, left)
+			}
+			c2 := s[left]
+			if needs[c2] > 0 {
+				windows[c2]--
+				if windows[c2] < needs[c2] {
+					match--
+				}
+			}
+			left++
+		}
+
 	}
 	return res
-
 }
 
 func main() {
 	//fmt.Println(findAnagrams("cbac", "bca"))
-	fmt.Println(findAnagrams("aabcaa", "abac"))
+	//fmt.Println(findAnagrams("aabcaa", "abac"))
 	//fmt.Println(findAnagrams("cbaebabacd", "bca"))
 	//fmt.Println(findAnagrams("abab", "ab"))
+	//fmt.Println(findAnagrams("baa", "aa"))
+	fmt.Println(findAnagrams("abbbaa", "abba"))
 }
