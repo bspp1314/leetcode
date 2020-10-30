e48e8d607616f11ad0fda822d4de3d8ac7f235d9
bspp1314 <linyuanpeng1314@gmail.com>
2020-10-30 18:36:16 +0800

    feat:update 474

--- 474/main.go
+++ 474/main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func findMaxForm(strs []string, m int, n int) int {
 	dp := make([][]int,m+1)
 	for i := 0; i < m+1; i++ {
@@ -39,7 +41,46 @@ func getOneNum(s string) []int {
 	return []int{n,len(s)-n}
 }
 
+func max(a, b int) int  {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
+func backpack(valuesAndWeight [2][]int,totalWeight int) int {
+	dp := make([][]int,len(valuesAndWeight[0]))
+	for i:=0;i<len(dp);i++ {
+		dp[i] = make([]int,totalWeight + 1 )
+	}
+
+	//放入第一个物品，填第一行列表
+	for j := 0;j <len(valuesAndWeight[0]);j++{
+		if valuesAndWeight[1][j] < totalWeight {
+			dp[0][j] = valuesAndWeight[0][j]
+		}
+	}
+
+
+	for i := 1 ; i < len(valuesAndWeight[0]) ; i++ {
+		for j := totalWeight;j > 0;j-- {
+			if j > valuesAndWeight[1][i]{
+				dp[i][j] = max(dp[i-1][j],dp[i-1][j-valuesAndWeight[1][i]] + valuesAndWeight[0][i])
+			}
+		}
+	}
+
+	return dp[len(valuesAndWeight[0])-1][totalWeight]
+}
+
 func main() {
+
+	out := backpack([2][]int{
+		[]int{12,3,10,3,6},
+		[]int{6,4,7,2,6},
+	},15)
+	fmt.Println(out)
 	//in := []string{"10","0001","111001","1","0"}
 	//out := findMaxForm(in, 3, 4)
 	//fmt.Println(out)
