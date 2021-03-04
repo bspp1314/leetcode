fdff56c32f306b52779786b7c1889057b64c1c0f
bspp1314 <linyuanpeng1314@gmail.com>
2021-03-04 23:51:00 +0800

    feat:udpate leetcode

--- 210/main.go
+++ 210/main.go
@@ -1,12 +1,54 @@
 package main
 //统计课程安排图中每个节点的入度，生成 入度表 indegrees。
-//借助一个队列 queue，将所有入度为 00 的节点入队。
+//借助一个队列 queue，将所有入度为 0 的节点入队。
 //当 queue 非空时，依次将队首节点出队，在课程安排图中删除此节点 pre：
-//并不是真正从邻接表中删除此节点 pre，而是将此节点对应所有邻接节点 cur 的入度 -1−1，即 indegrees[cur] -= 1。
-//当入度 -1−1后邻接节点 cur 的入度为 00，说明 cur 所有的前驱节点已经被 “删除”，此时将 cur 入队。
+//并不是真正从邻接表中删除此节点 pre，而是将此节点对应所有邻接节点 cur 的入度 -1，即 indegrees[cur] -= 1。
+//当入度 -1 后邻接节点 cur 的入度为 00，说明 cur 所有的前驱节点已经被 “删除”，此时将 cur 入队。
+
+
+
+//func canFinish(numCourses int, prerequisites [][]int) bool {
+//	var (
+//		edges = make([][]int, numCourses)
+//		indeg = make([]int, numCourses)
+//		result []int
+//	)
+//
+//	for _, info := range prerequisites {
+//		edges[info[1]] = append(edges[info[1]], info[0])
+//		indeg[info[0]]++
+//	}
+//
+//	q := []int{}
+//	for i := 0; i < numCourses; i++ {
+//		if indeg[i] == 0 {
+//			q = append(q, i)
+//		}
+//	}
+//
+//	for len(q) > 0 {
+//		u := q[0]
+//		q = q[1:]
+//		result = append(result, u)
+//		for _, v := range edges[u] {
+//			indeg[v]--
+//			if indeg[v] == 0 {
+//				q = append(q, v)
+//			}
+//		}
+//	}
+//	return len(result) == numCourses
+//}
 
 func findOrderBFS(numCourses int, prerequisites [][]int) []int {
 	//入度表
+	edges := make([][]int, numCourses)
+	indegree := make([]int,numCourses)
+	for _, info := range prerequisites {
+		edges[info[1]] = append(edges[info[1]], info[0])
+		indegree[info[0]]++
+	}
+
 
 }
 func findOrder(numCourses int, prerequisites [][]int) []int {
