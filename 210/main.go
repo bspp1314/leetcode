59b59836c914ef957a0d417ae902ce14914325bd
bspp1314 <linyuanpeng1314@gmail.com>
2021-03-05 23:43:48 +0800

    feat:add 210

--- 210/main.go
+++ 210/main.go
@@ -3,9 +3,10 @@ package main
 //借助一个队列 queue，将所有入度为 0 的节点入队。
 //当 queue 非空时，依次将队首节点出队，在课程安排图中删除此节点 pre：
 //并不是真正从邻接表中删除此节点 pre，而是将此节点对应所有邻接节点 cur 的入度 -1，即 indegrees[cur] -= 1。
-//当入度 -1 后邻接节点 cur 的入度为 00，说明 cur 所有的前驱节点已经被 “删除”，此时将 cur 入队。
+//当入度 -1 后邻接节点 cur 的入度为 0，说明 cur 所有的前驱节点已经被 “删除”，此时将 cur 入队。
 
 
+//
 
 //func canFinish(numCourses int, prerequisites [][]int) bool {
 //	var (
@@ -40,18 +41,46 @@ package main
 //	return len(result) == numCourses
 //}
 
-func findOrderBFS(numCourses int, prerequisites [][]int) []int {
+func findOrder(numCourses int, prerequisites [][]int) []int {
 	//入度表
 	edges := make([][]int, numCourses)
 	indegree := make([]int,numCourses)
 	for _, info := range prerequisites {
-		edges[info[1]] = append(edges[info[1]], info[0])
-		indegree[info[0]]++
+		edges[info[1]] = append(edges[info[1]], info[0]) //临界表
+		indegree[info[0]]++ // 入度表
 	}
 
+	queue := make([]int,0)
+
+	for i := 0; i < numCourses; i++ {
+		if indegree[i] == 0 {
+			queue = append(queue,i)
+		}
+	}
+
+	result := make([]int, 0)
+
+	for len(queue) != 0  {
+		u := queue[0]
+		queue = queue[1:]
+
+		result = append(result, u)
+		for _, v  := range edges[u] {
+			indegree[v]--
+			if indegree[v] == 0 {
+				queue = append(queue,v)
+			}
+		}
+	}
+
+	if len(result) != numCourses {
+		return []int{}
+	}
+
+	return result
 
 }
-func findOrder(numCourses int, prerequisites [][]int) []int {
+func findOrder2(numCourses int, prerequisites [][]int) []int {
 	//领接矩阵
 	edges := make([][]int, numCourses)
 	for _, info := range prerequisites {
