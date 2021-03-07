f81551f79e2cba17a510f088d2decbbeb17c8ac6
bspp1314 <linyuanpeng1314@gmail.com>
2021-03-07 19:22:18 +0800

    feat:add leetcode 219

--- 219/main.go
+++ 219/main.go
@@ -19,6 +19,31 @@ func containsNearbyDuplicate(nums []int, k int) bool {
 	return false
 }
 
+func containsNearbyDuplicate2(nums []int, k int) bool {
+	duplicateMap := make(map[int]int,0)
+
+	for i := 0; i < len(nums); i++ {
+		v,exist := duplicateMap[nums[i]]
+		if exist {
+			if (i - v) <= k {
+				return true
+			}
+		}
+
+		duplicateMap[nums[i]] = i
+	}
+
+	return false
+}
+
+func abs(a int) int {
+	if a < 0 {
+		return -a
+	}
+
+	return a
+}
+
 func main() {
 	fmt.Println(containsNearbyDuplicate([]int{1, 2, 3, 1, 2, 3}, 2))
 	fmt.Println(containsNearbyDuplicate([]int{99, 99}, 2))
