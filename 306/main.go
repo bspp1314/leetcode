18e1b3ea1eb13a1465f0d9103d12e4bc7d02c545
bspp1314 <linyuanpeng1314@gmail.com>
2021-04-20 09:39:57 +0800

    feat:add leetcode 306

---
+++ 306/main.go
@@ -0,0 +1,56 @@
+package main
+
+import "fmt"
+
+func isAdditiveNumber(num string) []int {
+	n := len(num)
+	var back  func(index int,first,second int,count int,sub []int)([]int,bool)
+	back = func(index int,first,second int, count int,sub []int) ([]int,bool) {
+		if index == n {
+			if count >= 3 {
+				return sub,true
+			}else{
+				return []int{},false
+			}
+		}
+
+		sum := first + second
+
+
+		value := 0
+		for i := index;i < n ;i++ {
+			// 除 0 以外，其他数字第一位不能为 0
+			if i > index && num[index] == '0' {
+				break
+			}
+
+			value = value * 10 + int(num[i] - '0')
+
+			if count >= 2 {
+				if value < sum {
+					// 小的话继续向后继续拼接
+					continue
+				} else if value > sum {
+					// 大的话直接结束，再往后拼接无意义
+					break
+				}
+			}
+
+			newSub := append(sub,value)
+
+			if res,v := back(i + 1, second, value, count + 1,newSub);v{
+				return res,v
+			}
+		}
+
+		return []int{},false
+	}
+
+	v,_ := back(0,0,0,0,[]int{})
+	return v
+}
+
+func main() {
+	fmt.Println(isAdditiveNumber("112358"))
+}
+
