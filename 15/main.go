1f2c4e9929778a3661beecb3052f93ae984c819e
bspp1314 <linyuanpeng1314@gmail.com>
2021-05-19 23:31:56 +0800

    feat:add leetcode 15

--- 15/main.go
+++ 15/main.go
@@ -50,6 +50,48 @@ func threeSum(nums []int) [][]int {
 	return res
 }
 
+func threeSum2(nums []int) [][]int {
+	res := make([][]int, 0)
+	if len(nums) < 2 {
+		return res
+	}
+
+	sort.Ints(nums)
+
+	slow := 0
+	for nums[slow] <= 0 && (slow+2) < len(nums) {
+		if slow > 0 && nums[slow] == nums[slow-1] {
+			continue
+		}
+
+		left := slow + 1
+		right := len(nums) - 1
+
+		for left < right {
+			k := nums[left] + nums[right] + nums[slow]
+			if k == 0 {
+				res = append(res, []int{slow, left, right})
+
+				for left < right && nums[left] == nums[left+1] {
+					left++
+				}
+
+				for left < right && nums[right] == nums[right-1] {
+					right--
+				}
+				left++
+				right--
+			} else if k < 0 {
+				left++
+			} else {
+				right--
+			}
+		}
+	}
+
+	return res
+}
+
 func main() {
 	fmt.Println(threeSum([]int{0, 0, 0}))
 }
