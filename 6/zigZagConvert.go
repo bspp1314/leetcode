22cdcbf6dcf66c46aaf88763cbe27996b0342213
bspp1314 <linyuanpeng1314@gmail.com>
2021-05-19 23:49:25 +0800

    feat:add leetcode 6

--- 6/zigZagConvert.go
+++ 6/zigZagConvert.go
@@ -51,14 +51,48 @@ func convert(s string, numRows int) string {
 }
 
 /*
-	LEETCODEISHIRING
-	L      D      R   idx = i + 2 * numRows -2
-	E    O E    I I	  i + 2 * numRows -2 -2 * row , 2 * row
-	E  C   I  H   N
-	T      S      G   indx = i + 2 * row -2
-
+	LEET CODE ISHI RING   //4
+	L      D      R   // 0 + 6    12
+	E    O E    I I  //  1 5 7 11 13
+	E  C   I  H   N      2 4 8 10  14
+	T      S      G      3   9    15
 */
+
+func convert3(s string, numRows int) string {
+	if numRows == 0 || len(s) <= 1 {
+		return s
+	}
+
+	resBytes := make([]byte, 0, len(s))
+	line := 0
+	n := len(s)
+
+	for line < numRows {
+		index := line
+		z := true
+		for index < n {
+			resBytes = append(resBytes, s[index])
+			if line == 0 || line == numRows-1 {
+				index += 2*numRows - 2
+			} else {
+				if z {
+					index += 2*numRows - 2 - line
+					z = false
+				} else {
+					index += line
+					z = true
+				}
+			}
+		}
+		line++
+	}
+
+	return string(resBytes)
+
+}
+
 func main() {
 	fmt.Println(convert("PAYPALISHIRING", 4))
 	fmt.Println(convert2("PAYPALISHIRING", 4))
+	fmt.Println(convert3("PAYPALISHIRING", 4))
 }
