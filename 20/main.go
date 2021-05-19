60bcc7ccdeedeb743a9de4d72748c01509ee189b
bspp1314 <linyuanpeng1314@gmail.com>
2021-05-19 23:35:23 +0800

    feat:add leetcode 20

--- 20/main.go
+++ 20/main.go
@@ -1,1 +1,50 @@
-package _0
+package main
+
+import "fmt"
+
+var m1 = map[byte]byte {
+	'{':'}',
+	'(':')',
+	'[':']',
+}
+
+var m2 = map[byte]byte {
+	'}':'{',
+	']':'[',
+	')':'(',
+}
+
+
+func isValid(s string) bool {
+	stack := make([]byte,0)
+
+
+	for i:=0;i<len(s);i++ {
+		if v,ok := m1[s[i]];ok {
+			stack = append(stack,v)
+		}else if _,ok := m2[s[i]];ok {
+			if len(stack) == 0 {
+				return false
+			}
+
+			if stack[len(stack)-1] != s[i] {
+				return false
+			}
+			stack = stack[:len(stack)-1]
+
+		}else{
+			return false
+		}
+	}
+
+	if len(stack) != 0 {
+		return false
+	}
+
+	return true
+}
+
+func main() {
+	fmt.Println(isValid("()"))
+	//fmt.Println(isValid("{{{(((())))}}"))
+}
