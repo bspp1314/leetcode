62470915b98384872ea473f5ff691edec09783ff
bspp1314 <linyuanpeng1314@gmail.com>
2021-05-19 23:52:38 +0800

    feat:add leetcode 9

--- 9/isPalindrom.go
+++ 9/isPalindrom.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 func isPalindrom(x int) bool {
 	if x < 0 {
@@ -25,9 +28,30 @@ func isPalindrom(x int) bool {
 	}
 }
 
+func isPalindrome(x int) bool {
+	if x < 0 {
+		return false
+	}
+
+	if x % 10 == 0 {
+		return false
+	}
+
+	t := x
+	val := 0
+	for x != 0 {
+		if math.MaxInt32/10 < val  {
+			return false
+		}
+		val = val * 10 + x % 10
+		x = x / 10
+	}
+	return t == val
+}
+
 func main() {
-	fmt.Println(isPalindrom(0))
-	fmt.Println(isPalindrom(1243421))
-	fmt.Println(isPalindrom(123321))
-	fmt.Println(isPalindrom(-123321))
+	//fmt.Println(isPalindrom(0))
+	//fmt.Println(isPalindrom(1243421))
+	//fmt.Println(isPalindrom(123321))
+	fmt.Println(isPalindrome(121))
 }
