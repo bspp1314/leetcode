ee30a73bfd3f6b0225df9fcc2e9cc6b50c24bc98
bspp1314 <linyuanpeng1314@gmail.com>
2021-05-23 19:53:01 +0800

    feat:add leetcode 32

--- 32/main.go
+++ 32/main.go
@@ -1,1 +1,38 @@
-package _2
+package main
+
+
+func longestValidParentheses(s string) int {
+	if len(s) <= 1 {
+		return 0
+	}
+
+	stack := make([]int,0)
+	stack = append(stack, -1)
+	res := 0
+
+	for i:=0;i <len(s);i++ {
+		if s[i] == '(' {
+			stack = append(stack,i)
+		}else{
+			stack = stack[:len(stack)-1]
+			if len(stack) == 0 {
+				stack = append(stack, i)
+			} else {
+				res = max(res, i - stack[len(stack)-1])
+			}
+		}
+	}
+
+	return res
+}
+
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
+func main() {
+
+}
