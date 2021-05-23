c930ef04adcb18522d4fba766f8d8985b8879e86
bspp1314 <linyuanpeng1314@gmail.com>
2021-05-23 19:54:44 +0800

    feat:add leetcode 38

--- 38/main.go
+++ 38/main.go
@@ -2,17 +2,6 @@ package main
 
 import "fmt"
 
-var byteToInt = map[byte]int{
-	'1':1,
-	'2':2,
-	'3':3,
-	'4':4,
-	'5':5,
-	'6':6,
-	'7':7,
-	'8':8,
-	'9':9,
-}
 
 func countAndSay(n int) string {
 	if n == 1 {
@@ -24,29 +13,30 @@ func countAndSay(n int) string {
 	}
 
 	v := countAndSay(n-1)
+	//1 1
+	//2 11
+	//3 21
+	//4 1211
+	//5 111221
 
-	byteMap := make(map[byte]int)
 	res := ""
-	for i:=0;i<len(v);i++{
-		if i == 0 {
-			byteMap[v[i]] = 1
+	c := v[0]
+	num := 1
+
+	for i:=1;i < len(v);i++ {
+		if v[i] == c {
+			num++
 		}else{
-			if byteMap[v[i]] == 0 {
-				k := byteMap[v[i-1]]
-				if k != 0 {
-					res += fmt.Sprintf("%d%d",k,byteToInt[v[i-1]])
-					delete(byteMap,v[i-1])
-				}
-			}
-			byteMap[v[i]]++
+			res+=fmt.Sprintf("%d%c",num,c)
+			c = v[i]
+			num = 1
 		}
 	}
 
-
-	k := byteMap[v[len(v)-1]]
-	res += fmt.Sprintf("%d%d",k,byteToInt[v[len(v)-1]])
+	res+=fmt.Sprintf("%d%c",num,c)
 
 	return res
+
 }
 
 
