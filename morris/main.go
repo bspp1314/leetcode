46f3cfa67a9ec0240fbd4a2150e03ebea22a6144
bspp1314 <linyuanpeng1314@gmail.com>
2021-06-06 19:52:56 +0800

    feat:add leetcode morris

--- morris/main.go
+++ morris/main.go
@@ -1,1 +1,33 @@
-package morris
+package main
+
+type TreeNode struct {
+	Left  *TreeNode
+	Right *TreeNode
+	Val   int
+}
+
+
+//  1 . 从根节点开始
+
+func morrisInOrderTraversal(root *TreeNode) []int {
+	node := root
+	var prev *TreeNode
+
+	res := make([]int,0)
+	for node != nil {
+		if node.Left == nil {
+			res = append(res,node.Val)
+			node = node.Right
+		}else{
+			//查询前驱节点
+			prev = node.Left
+
+		}
+	}
+
+	return res
+}
+
+func main() {
+
+}
