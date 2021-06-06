0a14a07409925d2b95d9f1cc13033ab8416b1f7f
bspp1314 <linyuanpeng1314@gmail.com>
2021-06-06 19:52:17 +0800

    feat:add leetcode 60

--- 60/main.go
+++ 60/main.go
@@ -2,39 +2,28 @@ package main
 
 import "fmt"
 
-var int2Byte = map[int]byte{
-	1: '1',
-	2: '2',
-	3: '3',
-	4: '4',
-	5: '5',
-	6: '6',
-	7: '7',
-	8: '8',
-	9: '9',
-}
 
 func getPermutation(n int, k int) string {
-
-	fac := make([]int, n+1)
-	fac[0] = 1
-	can := make([]int, n)
+	factorial := make([]int, n+1)
+	factorial[0] = 1
+	candidates := make([]int,n)
 	for i := 1; i <= n; i++ {
-		fac[i] = fac[i-1] * i
-		can[i-1] = i
+		factorial[i] = factorial[i - 1] * i
+		candidates[i-1] = i
+	}
+	k--
+	fmt.Println(candidates)
+
+	sb := make([]byte,0)
+	for i := n-1;i >=0;i--{
+		index := k / factorial[i]
+		sb = append(sb,byte('0'+candidates[index]))
+		candidates = append(candidates[0:index],candidates[index+1:]...)
+		k = k - index * factorial[i]
 	}
 
-	k = k - 1
-	var res []byte
-	for i := n - 1; i >= 0; i-- {
-		index := k / fac[i]
-		res = append(res, int2Byte[can[index]])
-		can = append(can[0:index],can[index+1:]...)
-		k -= index * fac[i]
 
-	}
-
-	return string(res)
+	return string(sb)
 
 }
 
