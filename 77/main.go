306792e70ce5842b3863fc3e77d18dcf360906e5
bspp1314 <linyuanpeng1314@gmail.com>
2021-06-06 19:52:37 +0800

    feat:add leetcode 77

--- 77/main.go
+++ 77/main.go
@@ -2,58 +2,41 @@ package main
 
 import "fmt"
 
-func combine(n int, k int) [][]int {
+
+// n k
+
+func combine(n int,k int)[][]int  {
 	var res [][]int
-	dfs(&res,0,n,k,make([]int,0))
-	return res
-}
 
-func dfs(res *[][]int,left int,n int,k int,q []int) {
-	if k > (n-left) {
-		return
-	}
+	var dfs func(sub []int,left int)
 
-	if k == 1 {
-		for i := left;i < n;i++ {
-			tem := make([]int,len(q)+1)
-			copy(tem,q)
-			tem[len(tem)-1] = i+1
-			*res = append(*res,tem)
+
+	dfs = func(sub []int, left int) {
+		if (n -left +1)  < k  - len(sub){
+			return
 		}
 
-		return
+		if len(sub) == k {
+			temp := make([]int,len(sub))
+			copy(temp,sub)
+			res = append(res,temp)
+			return
+		}
+
+		for i := left; i <= n ; i++ {
+			newSub := append(sub,i)
+			dfs(newSub,i+1)
+		}
 	}
 
-	for i:=left;i<n;i++{
-		dfs(res,i+1,n,k-1,append(q,i+1))
-	}
-}
+	dfs([]int{},1)
 
-func combine2(n int, k int) [][]int {
-	var res [][]int
-	dfs2([]int{},0,n,k,&res)
 	return res
-}
-
-func dfs2(subNumbs []int,left,n,k int,res *[][]int) {
-	if (n - left) < k - len(subNumbs) {
-		return
-	}
-
-	if len(subNumbs) == k {
-		temp := make([]int,len(subNumbs))
-		copy(temp,subNumbs)
-		*res = append(*res,temp)
-		return
-	}
-
-	for i := left; i < n; i++ {
-		dfs2(append(subNumbs,i+1),i+1,n,k,res)
-	}
 
 }
+
+
 
 func main() {
 	fmt.Println( combine(5,2))
-	fmt.Println( combine2(5,2))
 }
