531ac1ca35d678d151c39c6987ff22c3b1bbc32a
bspp1314 <linyuanpeng1314@gmail.com>
2021-06-06 19:52:34 +0800

    feat:add leetcode 75

--- 75/main.go
+++ 75/main.go
@@ -1,27 +1,26 @@
 package main
 
 func sortColors(nums []int)  {
-
-	leftSlow := 0
-	leftFast := 0
+	slow := 0
+	fast := 0
 	right := len(nums) -1
 
-	leftSlow = leftFast
-
-	for leftFast <= right {
-		if nums[leftFast] == 2 {
-			nums[leftFast],nums[right] = nums[right],nums[leftFast]
-			right --
-		}else if nums[leftFast] == 1 {
-			leftFast++
-		}else {
-			nums[leftFast],nums[leftSlow] = nums[leftSlow],nums[leftFast]
-			leftFast++
-			leftSlow++
+	for fast <= right {
+		if nums[fast] == 0 {
+			nums[fast],nums[slow] = nums[slow],nums[fast]
+			slow++
+			fast++
+		}else if nums[fast] == 2 {
+			nums[fast],nums[right] = nums[right],nums[fast]
+			right--
+		}else{
+			fast++
 		}
 	}
-}
 
+
+}
+// 2 2 2 1 1 1 0 0 0
 func main()  {
 	sortColors([]int{0,0,0,1,1,1,2,2,2})
 }
