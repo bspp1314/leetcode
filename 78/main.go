45d0809d890f2a7936c22c93b7a751b8ff93a956
bspp1314 <linyuanpeng1314@gmail.com>
2021-06-06 19:52:41 +0800

    feat:add leetcode 78

--- 78/main.go
+++ 78/main.go
@@ -1,1 +1,28 @@
-package _8
+package main
+
+import "fmt"
+
+func subsets(nums []int) [][]int {
+	if len(nums) == 0 {
+		return [][]int{[]int{}}
+	}
+
+	numsLastIndex := len(nums)-1
+	sub := subsets(nums[:numsLastIndex])
+
+	subLen := len(sub)
+
+	for i := 0; i < subLen; i++ {
+		tem := make([]int,len(sub[i]))
+		copy(tem,sub[i])
+		sub = append(sub,append(tem,nums[numsLastIndex]))
+	}
+
+
+	return sub
+
+}
+
+func main() {
+	fmt.Println(subsets([]int{9,0,3,5,7}))
+}
