17caee5a42e2c2103ae944d2548124f062ae8d4f
bspp1314 <linyuanpeng1314@gmail.com>
2021-06-06 19:52:13 +0800

    feat:add leetcode 120

--- 120/main.go
+++ 120/main.go
@@ -1,1 +1,29 @@
-package _20
+package main
+
+func minimumTotal(triangle [][]int) int {
+	if len(triangle) == 0 {
+		return 0
+	}
+
+	for i := len(triangle) - 2; i >= 0; i-- {
+		for j := 0; j < len(triangle[i]); j++ {
+			if triangle[i+1][j] > triangle[i+1][j+1] {
+				triangle[i][j] += triangle[i+1][j+1]
+			} else {
+				triangle[i][j] += triangle[i+1][j]
+			}
+		}
+	}
+
+	return triangle[0][0]
+
+}
+
+func main() {
+	minimumTotal([][]int{
+		{2},
+		{3, 4},
+		{6, 5, 7},
+		{4, 1, 8, 3},
+	})
+}
