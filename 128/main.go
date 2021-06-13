0306ec568c091e8d327a3a67833928fcc59d1e37
bspp1314 <linyuanpeng1314@gmail.com>
2021-06-13 17:50:47 +0800

    feat:add leetcode 128

--- 128/main.go
+++ 128/main.go
@@ -1,1 +1,41 @@
-package _28
+package main
+
+func longestConsecutive(nums []int) int {
+	if len(nums) <= 1 {
+		return len(nums)
+	}
+
+	numsSet := make(map[int]bool)
+
+	for _, num := range nums {
+		numsSet[num] = true
+	}
+
+	res := 0
+
+	for num, _ := range numsSet {
+		if !numsSet[num-1] {
+			beginNum := num+1
+			newRes  := 1
+
+			for numsSet[beginNum] {
+				beginNum++
+				newRes++
+			}
+
+			if res < newRes {
+				res = newRes
+			}
+		}
+	}
+
+	return res
+
+}
+
+
+
+
+func main() {
+
+}
