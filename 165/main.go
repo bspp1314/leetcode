f5dd9a5e2eedc62c43b39a34b80661da0bd6b665
bspp1314 <linyuanpeng1314@gmail.com>
2021-07-08 22:29:13 +0800

    feat:add leetcode 165

--- 165/main.go
+++ 165/main.go
@@ -6,9 +6,58 @@ import (
 	"strings"
 )
 
+func getNextValue(version string,begin,l int) (int,int)   {
+	for begin  < l && version[begin] == '0'  {
+		 begin++
+	}
+	if begin >= l {
+		return 0,begin
+	}
+
+	res := 0
+
+	for begin < l  {
+		if version[begin] == '.' {
+			for begin < l && version[begin] == '.' {
+				begin++
+			}
+			break
+		}else{
+			res = res * 10 + int(version[begin] - '0')
+			begin++
+		}
+	}
+
+	return res,begin
+}
+
 func compareVersion(version1 string, version2 string) int {
+	begin1 := 0
+	begin2 := 0
+	len1 := len(version1)
+	len2 := len(version2)
+	v1 := 0
+	v2 := 0
+
+	for begin1 < len1 || begin2 < len2 {
+		v1,begin1 = getNextValue(version1,begin1,len1)
+		v2,begin2 = getNextValue(version2,begin2,len2)
+
+		if v1 > v2 {
+			return 1
+		}else if v2 > v1 {
+			return -1
+		}
+	}
+
+	return 0
+
+}
+
+func compareVersion2(version1 string, version2 string) int {
 	v1 := strings.Split(version1,".")
 	v2 := strings.Split(version2,".")
+
 	v1Len := len(v1)
 	v2Len := len(v2)
 	maxLen := v1Len
@@ -39,12 +88,10 @@ func compareVersion(version1 string, version2 string) int {
 		}
 	}
 
-
-
-
 	return 0
 }
 
 func main() {
-	fmt.Println(compareVersion("1.1.1","1.01.01"))
+
+	fmt.Println(compareVersion2("1.1","1.1.0"))
 }
