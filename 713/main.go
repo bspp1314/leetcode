4d5913d7492a1d62e02b9491bac88fddd81c7e26
bspp1314 <linyuanpeng1314@gmail.com>
2021-07-08 22:35:08 +0800

    feat:add leetcode 713

--- 713/main.go
+++ 713/main.go
@@ -1,1 +1,39 @@
-package _13
+package main
+
+func numSubarrayProductLessThanK(nums []int, k int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+
+	if k <= 1 {
+		return 0
+	}
+
+	left := 0
+	product := 1
+	res := 0
+
+	for right := 0; right < len(nums); right++ {
+		product = product * nums[right]
+		for product >= k {
+			product = product / nums[left]
+			left++
+		}
+
+		res += right - left + 1
+	}
+
+	return res
+}
+
+
+// 5 2 3 4 5
+// 5
+// 52 2
+// 523 23 3
+// 5234 2 3 4
+// 2345 345 35 5
+
+func main() {
+	numSubarrayProductLessThanK([]int{1,2,3}, 1)
+}
