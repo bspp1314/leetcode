27d6ac78e91f940f3e6e3dd1942d56cbca1bc90c
bspp1314 <linyuanpeng1314@gmail.com>
2021-07-08 22:27:17 +0800

    feat:add leetcode 145

--- 145/main.go
+++ 145/main.go
@@ -1,1 +1,48 @@
-package _45
+package main
+
+
+//前 中左右
+//中 左中右
+//后 左右中
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func postorderTraversal(root *TreeNode) []int {
+	if root == nil {
+		return []int{}
+	}
+
+	var res []int
+
+	stack := make([]*TreeNode,0)
+	//用来记录左节点是否遍历过
+	var pre *TreeNode
+
+	for root !=nil || len(stack) != 0 {
+		for root != nil {
+			stack = append(stack,root)
+			root = root.Left
+		}
+
+		n := stack[len(stack)-1]
+		stack = stack[:len(stack)-1]
+
+		if n.Right == nil || n.Right == pre {
+			pre = n
+			root = nil
+			res = append(res,n.Val)
+		}else{
+			stack = append(stack,n)
+			root = n.Right
+		}
+	}
+
+	return res
+}
+func main()  {
+
+}
