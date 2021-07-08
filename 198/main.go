9b8d8b3d09083c3d8748dc29617cbefe09aea355
bspp1314 <linyuanpeng1314@gmail.com>
2021-07-08 22:34:55 +0800

    feat:add leetcode 198

--- 198/main.go
+++ 198/main.go
@@ -1,6 +1,18 @@
 package main
 
 import "fmt"
+// 偷窃第 k 间房屋，那么就不能偷窃第 k−1 间房屋，偷窃总金额为前 k-2 间房屋的最高总金额与第 k 间房屋的金额之和。
+
+// 不偷窃第 k 间房屋，偷窃总金额为前 k−1 间房屋的最高总金额。
+
+
+func Max(a,b int) int   {
+	if a > b {
+		return a
+	}
+
+	return b
+}
 
 func rob(nums []int) int {
 	if len(nums) == 0 {
@@ -11,19 +23,13 @@ func rob(nums []int) int {
 		return nums[0]
 	}
 
-	max := func(a,b int) int {
-		if a > b {
-			return a
-		}
 
-		return b
-	}
 
 	dp0 := nums[0]
-	dp1 := max(nums[0],nums[1])
+	dp1 := Max(dp0,nums[1])
 
-	for i := 2; i < len(nums); i++ {
-		new := max(dp0+nums[i],dp1)
+	for i := 1; i < len(nums); i++ {
+		new := Max(dp0+nums[i],dp1)
 		dp0 = dp1
 		dp1 = new
 
