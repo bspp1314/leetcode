25814acc1a7014ee3bae664d30c54035bffec3e6
bspp1314 <linyuanpeng1314@gmail.com>
2021-07-08 22:25:44 +0800

    feat:add leetcode 141

--- 141/main.go
+++ 141/main.go
@@ -4,42 +4,27 @@ type ListNode struct {
 	Next *ListNode
 }
 
-func hasCycle2(head *ListNode) bool {
-	if head == nil {
-		return false
-	}
-
-	address := make(map[*ListNode]bool)
-	for head != nil {
-		if address[head] {
-			return true
-		}
-		address[head] = true
-		head = head.Next
-
-	}
-	return false
-}
 
 func hasCycle(head *ListNode) bool {
 	if head == nil || head.Next == nil {
 		return false
 	}
 
-	slow := head
-	fast := head
+	left := head
+	right := head
 	for {
-		if fast == nil {
+		left = left.Next
+		right = right.Next
+		if right == nil {
 			return false
 		}
-		slow = slow.Next
-		fast = fast.Next
-		if fast == nil {
-			return false
-		}
-		fast = fast.Next
 
-		if slow == fast {
+		right = right.Next
+		if right == nil {
+			return false
+		}
+
+		if left == right {
 			break
 		}
 	}
