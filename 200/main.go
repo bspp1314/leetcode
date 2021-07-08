761e59940ca66f8b6233767e21d7d3c7aeb82cf2
bspp1314 <linyuanpeng1314@gmail.com>
2021-07-08 22:35:03 +0800

    feat:add leetcode 200

--- 200/main.go
+++ 200/main.go
@@ -1,6 +1,5 @@
 package main
 
-import "fmt"
 
 func numIslands2(grid [][]byte) int {
 	if len(grid) == 0 || len(grid[0]) == 0 {
@@ -40,50 +39,41 @@ func numIslands2(grid [][]byte) int {
 	return res
 }
 
+// 深度优先遍历
 func numIslands(grid [][]byte) int {
 	if len(grid) == 0 || len(grid[0]) == 0 {
 		return 0
 	}
 
+	m := len(grid)
+	n := len(grid[0])
+
+	var dfs func(i,j int)
+
+	dfs = func(i, j int) {
+		if i < 0 || j < 0|| i >= m || j >= n || grid[i][j] == '0'{
+			return
+		}
+
+		grid[i][j] = '0'
+
+		dfs(i+1,j)
+		dfs(i-1,j)
+		dfs(i,j+1)
+		dfs(i,j-1)
+
+
+	}
+
 	res := 0
-	for i := 0; i < len(grid); i++ {
-		for j := 0; j < len(grid[i]); j++ {
+	for i := 0; i < m; i++ {
+		for j := 0; j < n; j++ {
 			if grid[i][j] == '1' {
 				res++
-				dfs(grid, i, j)
+				dfs(i,j)
 			}
 		}
 	}
 
 	return res
-}
-
-func dfs(grid [][]byte, i, j int) {
-	if i < 0 || j < 0 || i >= len(grid) || j >= len(grid[0]) {
-		return
-	}
-
-	if grid[i][j] == '0' {
-		return
-	}
-
-	grid[i][j] = '0'
-
-	dfs(grid, i+1, j)
-	dfs(grid, i-1, j)
-	dfs(grid, i, j-1)
-	dfs(grid, i, j+1)
-}
-
-func main() {
-	land := [][]byte{
-		{'1', '1', '0', '1'},
-		{'1', '1', '0', '1'},
-		{'1', '1', '0', '1'},
-		{'1', '1', '0', '1'},
-	}
-
-	out := numIslands2(land)
-
-	fmt.Println(out)
 }
