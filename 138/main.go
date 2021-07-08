aa3aa228dd11aa9115e1b714ae4e0c81fd054e06
bspp1314 <linyuanpeng1314@gmail.com>
2021-07-08 22:23:52 +0800

    feat:add leetcode 138

--- 138/main.go
+++ 138/main.go
@@ -8,34 +8,86 @@ type Node struct {
 
 
 
-var Visit 	 = make(map[*Node]*Node)
 func copyRandomList(head *Node) *Node {
 	if head == nil {
 		return nil
 	}
 
-	if v,ok := Visit[head];ok {
-		return v
+	var dfs  func(node *Node) *Node
+
+	vis := make(map[*Node]*Node)
+
+	dfs = func(node *Node) *Node {
+		if node == nil {
+			return nil
+		}
+
+		copyNode,ok := vis[node]
+		if ok {
+			return copyNode
+		}
+
+
+
+		copyNode = &Node{
+			Val:node.Val,
+		}
+
+		vis[node] = copyNode
+
+		copyNode.Next = dfs(node.Next)
+		copyNode.Random = dfs(node.Random)
+
+		return copyNode
 	}
 
-	if head.Next == nil || head.Random == nil {
-		return &Node{Val: head.Val}
+	return dfs(head)
+}
+
+func copyRandomList3(head *Node) *Node {
+	if head == nil {
+		return nil
 	}
 
-	copyNode := &Node{
-		Val:    head.Val,
-		Next:   nil,
-		Random: nil,
-	}
-	Visit[head] = copyNode
-	if head.Next != nil {
-		copyNode.Next = copyRandomList(head.Next)
+
+
+	vis := make(map[*Node]*Node)
+	vis[head] = &Node{Val:head.Val}
+	q := []*Node{head}
+
+	for len(q) != 0 {
+		n := q[0]
+		q = q[1:len(q)]
+
+
+
+		if n.Next != nil {
+			_,ok := vis[n.Next]
+			if !ok {
+				cn := &Node{Val:n.Next.Val}
+				vis[n.Next]= cn
+				q = append(q,n.Next)
+			}
+		}
+		vis[q[0]].Next = vis[n.Next]
+
+		if n.Random != nil {
+			_,ok := vis[n.Random]
+			if !ok {
+				cn := &Node{Val:n.Random.Val}
+				vis[n.Random]= cn
+				q = append(q,n.Random)
+			}
+		}
+
+		vis[q[0]].Random= vis[n.Random]
+
+
 	}
 
-	if head.Random != nil {
-		copyNode.Random = copyRandomList(head.Random)
-	}
 
-	return copyNode
+	return vis[head]
+}
 
+func main() {
 }
