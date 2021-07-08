799b46f289ba77cda8d22bf8c54dab5e2a08e8b3
bspp1314 <linyuanpeng1314@gmail.com>
2021-07-08 22:25:40 +0800

    feat:add leetcode 140

--- 140/main.go
+++ 140/main.go
@@ -1,38 +1,49 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"strings"
 )
+
+
 func wordBreak(s string, wordDict []string) (sentences []string) {
-	wordSet := map[string]struct{}{}
+	wordSet := make(map[string]bool)
 	for _, w := range wordDict {
-		wordSet[w] = struct{}{}
+		wordSet[w] = true
 	}
 
 	n := len(s)
-	dp := make([][][]string, n)
+	dp := make([][][]string, n+1)
 	var backtrack func(index int) [][]string
+	// 对于字符串 ss，如果某个前缀是单词列表中的单词，则拆分出该单词，然后对 s 的剩余部分继续拆分。
+
 	backtrack = func(index int) [][]string {
 		if dp[index] != nil {
 			return dp[index]
 		}
-		wordsList := [][]string{}
-		for i := index; i < n; i++ {
-			word := s[index:i+1]
-			if _, has := wordSet[word]; has {
-				for _, nextWords := range backtrack(i+1) {
-					wordsList = append(wordsList, append([]string{word}, nextWords...))
+
+		var wordsList [][]string
+
+		for i := index+1;i <= n;i++ {
+			if wordSet[s[index:i]] {
+				if i == n {
+					wordsList = append(wordsList, []string{s[index:i]})
+				}else{
+					nextWordLists := backtrack(i)
+					for _, list := range nextWordLists {
+						wordsList = append(wordsList,append([]string{s[index:i]},list...))
+					}
 				}
 			}
 		}
-		//word := s[index:]
-		//if _, has := wordSet[word]; has {
-		//	wordsList = append(wordsList, []string{word})
-		//}
+
 		dp[index] = wordsList
 		return wordsList
 	}
+
+
+
 	for _, words := range backtrack(0) {
 		sentences = append(sentences, strings.Join(words, " "))
 	}
@@ -40,12 +51,9 @@ func wordBreak(s string, wordDict []string) (sentences []string) {
 }
 
 
-
-
 func main() {
 	s := wordBreak("cat",[]string{"a","cat"})
-	s = wordBreak3("cat",[]string{"a","cat"})
-	log.Println(s)
+	fmt.Println("s is ",s)
 	for i:=0;i<len(s);i++ {
 		log.Println(s[i])
 	}
