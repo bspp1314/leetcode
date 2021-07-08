8d5e1670edb7ac6adbde279745dd4de686a46904
bspp1314 <linyuanpeng1314@gmail.com>
2021-07-08 22:23:47 +0800

    feat:add leetcode 137

--- 137/main.go
+++ 137/main.go
@@ -2,24 +2,31 @@ package main
 
 import "fmt"
 
-// (0,0) 0 (0,0)
-// (0,1) 0 (0,1)
-// (1,0) 0 (1,0)
-// (0,0) 1 (0,0)
-// (0,1) 1 (0,1)
-// (1,0) 1 (1,0)
+// 一个二进制位只能表示0或者1。也就是天生可以记录一个数出现了一次还是两次。
+// x ^ 0 = x
+// x ^ x = 0
 
-// newA = a & ~b & ~n | ~a & b & n
-// newB = ~a & b & ~n | ~a & ~b & n
+// 要记录出现3次，需要两个二进制位。那么上面单独的x就不行了。我们需要两个变量，每个变量取一位
 
+// ab ^ 00 = ab
+// ab ^ ab = 00
+
+//b = x &
+
+
+//x = x[7] x[6] x[5] x[4] x[3] x[2] x[1] x[0]
+//x = (a[7]b[7]) (a[6]b[6]) ... (a[1]b[1]) (a[0]b[0])
+
+//它是一个逻辑电路，a、b变量中，相同位置上，分别取出一位，负责完成00->01->10->00，也就是开头的那句话，当数字出现3次时置零。
 func singleNumber(nums []int) int {
 	a := 0
 	b := 0
-	for n := range nums {
-		newA := a & ^b & ^n | ^a & b & n
-		newB := ^a & b & ^n | ^a & ^b & n
+	for _,x := range nums {
+		b = (b ^ x) & (^a)
+		a = (a ^ x) & (^b)
 
-		newA,newB = newB,newA
+		fmt.Println("a",a)
+		fmt.Println("b",b)
 	}
 
 	return b
@@ -27,24 +34,6 @@ func singleNumber(nums []int) int {
 }
 
 func main() {
-	fmt.Println(singleNumber([]int{1, 1, 1, 3}))
-	//fmt.Printf("(%d,%d) ==> (%d,%d)\n",a,b,a & ^b  ^ 0,^a & b  ^ 0)
-	//a = 0
-	//b = 1
-	//fmt.Printf("(%d,%d) ==> (%d,%d)\n",a,b,a & ^b  ^ 0,^a & b  ^ 0)
-	//a = 1
-	//b = 0
-	//fmt.Printf("(%d,%d) ==> (%d,%d)\n",a,b,a & ^b  ^ 0,^a & b  ^ 0)
-	//
-
-	//a = 0
-	//b = 0
-	//fmt.Printf("(%d,%d) ==> (%d,%d)\n",a,b,a & ^b  ^ ^1,10)
-	//a = 0
-	//b = 1
-	//fmt.Printf("(%d,%d) ==> (%d,%d)\n",a,b,a & ^b  ^ ^1,10)
-	//a = 1
-	//b = 0
-	//fmt.Printf("(%d,%d) ==> (%d,%d)\n",a,b,a & ^b  ^ ^1,10)
+	fmt.Println(singleNumber([]int{1, 1, 1}))
 
 }
