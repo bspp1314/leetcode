04f9a276ed1b7f4c0237495c4f24b7718b46ec4b
bspp1314 <linyuanpeng1314@gmail.com>
2021-07-08 22:32:00 +0800

    feat:add leetcode 172

--- 172/main.go
+++ 172/main.go
@@ -1,1 +1,24 @@
-package _72
+package main
+
+// 5
+// 5 10  15  20  25
+// 1  1   1  1   2
+
+func trailingZeroes(n int) int {
+	if n < 5 {
+		return 0
+	}
+
+	res := 0
+	for n >= 5 {
+		res += n / 5
+		n = n / 5
+	}
+
+	return res
+
+}
+
+func main()  {
+
+}
