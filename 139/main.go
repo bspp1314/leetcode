cec9924bef68004a4a6c7bdc8345629064f6c9b6
bspp1314 <linyuanpeng1314@gmail.com>
2021-07-08 22:25:35 +0800

    feat:add leetcode 139

--- 139/main.go
+++ 139/main.go
@@ -1,1 +1,39 @@
-package _39
+package main
+
+func wordBreak(s string, wordDict []string) bool {
+	if len(s) == 0 {
+		return true
+	}
+
+	if len(wordDict) == 0 {
+		return false
+	}
+
+	wordDictMap := make(map[string]bool)
+
+	for i := 0;i < len(wordDict);i++ {
+		wordDictMap[wordDict[i]] = true
+	}
+
+
+	//dp[i]
+
+	dp := make([]bool, len(s) + 1)
+	dp[0] = true
+
+	for i := 1; i <= len(s);i++ {
+		for j := 0;j < i;j++ {
+			if dp[j] && wordDictMap[s[j:i]] {
+				dp[i] = true
+				break
+			}
+		}
+	}
+
+	return dp[len(s)]
+
+}
+
+func main() {
+	
+}
