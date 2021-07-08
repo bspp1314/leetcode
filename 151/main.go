56025948bd6a0c6aed43e868e3ad5f1b35f8362d
bspp1314 <linyuanpeng1314@gmail.com>
2021-07-08 22:27:38 +0800

    feat:add leetcode 151

--- 151/main.go
+++ 151/main.go
@@ -1,1 +1,44 @@
-package _51
+package main
+
+import "fmt"
+
+func reverseWords(s string) string {
+	var res []byte
+	left := 0
+	right := len(s) - 1
+
+	for left <= right && s[left] == ' ' {
+		left++
+	}
+
+	for left <= right && s[right] == ' ' {
+		right--
+	}
+
+	if left > right {
+		return ""
+	}
+
+	end := right + 1
+	for left <= right {
+		if s[right] == ' ' {
+			res = append(res, []byte(s[right+1:end])...)
+			res = append(res, ' ')
+			for left <= right && s[right] == ' ' {
+				right--
+				end = right + 1
+			}
+		} else {
+			right--
+		}
+	}
+
+	res = append(res, []byte(s[right+1:end])...)
+
+	return string(res)
+
+}
+
+func main() {
+	fmt.Println(reverseWords("the ske is blue"))
+}
